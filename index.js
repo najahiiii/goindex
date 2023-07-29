5b73c63d25548e491842c366f0bbddd6f80cffdf
Ahmad Thoriq Najahi <najahi@zephyrus.id>
2023-07-29 17:51:47 +0700

    chore: reformat
    Signed-off-by: Ahmad Thoriq Najahi <najahi@zephyrus.id>

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 var authConfig = {
-	"siteName": "",
-	"hash" : "",
-	"client_id": "",
-	"client_secret": "",
-	"refresh_token": "",
-	"root": "",
-	"gh_user": "",
-	"repo_name": "",
-	"favicon": ""
-};
+    siteName: '',
+    hash: '',
+    client_id: '',
+    client_secret: '',
+    refresh_token: '',
+    root: '',
+    gh_user: '',
+    repo_name: '',
+    favicon: '',
+}
 
-let gd;
+let gd
 
 const html = `
 	<!DOCTYPE html>
@@ -23,246 +23,266 @@ const html = `
 	<body>
 	</body>
 	</html>
-	`;
+	`
 
 addEventListener('fetch', (event) => {
-	event.respondWith(handleRequest(event.request));
-});
+    event.respondWith(handleRequest(event.request))
+})
 
 /**
-* Fetch a request
-* @param {Request} request
-*/
+ * Fetch a request
+ * @param {Request} request
+ */
 async function handleRequest(request) {
-	if (gd == undefined) {
-		gd = new googleDrive(authConfig);
-	}
+    if (gd == undefined) {
+        gd = new googleDrive(authConfig)
+    }
 
-	if (request.method == 'POST') {
-		return apiRequest(request);
-	}
+    if (request.method == 'POST') {
+        return apiRequest(request)
+    }
 
-	const url = new URL(request.url);
-	const path = url.pathname;
-	const action = url.searchParams.get('a');
+    const url = new URL(request.url)
+    const path = url.pathname
+    const action = url.searchParams.get('a')
 
-	if (path.substr(-1) == '/' || action != null) {
-		return new Response(html, {
-			status: 200, headers: { 'Content-Type': 'text/html; charset=utf-8' }
-		});
-	} else {
-		const file = await gd.file(path);
-		const range = request.headers.get('Range');
-		return gd.down(file.id, range);
-	}
+    if (path.substr(-1) == '/' || action != null) {
+        return new Response(html, {
+            status: 200,
+            headers: { 'Content-Type': 'text/html; charset=utf-8' },
+        })
+    } else {
+        const file = await gd.file(path)
+        const range = request.headers.get('Range')
+        return gd.down(file.id, range)
+    }
 }
 
 async function apiRequest(request) {
-	const url = new URL(request.url);
-	const path = url.pathname;
+    const url = new URL(request.url)
+    const path = url.pathname
 
-	const option = { status: 200, headers: { 'Access-Control-Allow-Origin': '*' } };
+    const option = {
+        status: 200,
+        headers: { 'Access-Control-Allow-Origin': '*' },
+    }
 
-	if (path.substr(-1) == '/') {
-		const list = await gd.list(path);
-		return new Response(JSON.stringify(list), option);
-	} else {
-		const file = await gd.file(path);
-		const range = request.headers.get('Range');
-		return new Response(JSON.stringify(file));
-	}
+    if (path.substr(-1) == '/') {
+        const list = await gd.list(path)
+        return new Response(JSON.stringify(list), option)
+    } else {
+        const file = await gd.file(path)
+        const range = request.headers.get('Range')
+        return new Response(JSON.stringify(file))
+    }
 }
 
 class googleDrive {
-	constructor(authConfig) {
-		this.authConfig = authConfig;
-		this.paths = [];
-		this.files = [];
-		this.passwords = [];
-		this.paths['/'] = authConfig.root;
-		this.accessToken();
-	}
+    constructor(authConfig) {
+        this.authConfig = authConfig
+        this.paths = []
+        this.files = []
+        this.passwords = []
+        this.paths['/'] = authConfig.root
+        this.accessToken()
+    }
 
-	async down(id, range = '') {
-		const url = `https://www.googleapis.com/drive/v3/files/${id}?alt=media&acknowledgeAbuse=true`;
-		const requestOption = await this.requestOption();
-		requestOption.headers['Range'] = range;
-		return await fetch(url, requestOption);
-	}
+    async down(id, range = '') {
+        const url = `https://www.googleapis.com/drive/v3/files/${id}?alt=media&acknowledgeAbuse=true`
+        const requestOption = await this.requestOption()
+        requestOption.headers['Range'] = range
+        return await fetch(url, requestOption)
+    }
 
-	async file(path) {
-		if (typeof this.files[path] == 'undefined') {
-			this.files[path] = await this._file(path);
-		}
-		return this.files[path];
-	}
+    async file(path) {
+        if (typeof this.files[path] == 'undefined') {
+            this.files[path] = await this._file(path)
+        }
+        return this.files[path]
+    }
 
-	async _file(path) {
-		const arr = path.split('/');
-		let name = arr.pop();
-		name = decodeURIComponent(name).replace(/\'/g, '\\\'');
-		const dir = arr.join('/') + '/';
-		const parent = await this.findPathId(dir);
-		let url = 'https://www.googleapis.com/drive/v3/files';
-		const params = { 'includeItemsFromAllDrives': true, 'supportsAllDrives': true };
-		params.q = `'${parent}' in parents and name = '${name}' and trashed = false and mimeType != 'application/vnd.google-apps.shortcut'`;
-		params.fields = 'files(id, name, mimeType, size , createdTime, modifiedTime)';
-		url += '?' + this.enQuery(params);
-		const requestOption = await this.requestOption();
-		const response = await fetch(url, requestOption);
-		const obj = await response.json();
-		return obj.files[0];
-	}
+    async _file(path) {
+        const arr = path.split('/')
+        let name = arr.pop()
+        name = decodeURIComponent(name).replace(/\'/g, "\\'")
+        const dir = arr.join('/') + '/'
+        const parent = await this.findPathId(dir)
+        let url = 'https://www.googleapis.com/drive/v3/files'
+        const params = {
+            includeItemsFromAllDrives: true,
+            supportsAllDrives: true,
+        }
+        params.q = `'${parent}' in parents and name = '${name}' and trashed = false and mimeType != 'application/vnd.google-apps.shortcut'`
+        params.fields =
+            'files(id, name, mimeType, size , createdTime, modifiedTime)'
+        url += '?' + this.enQuery(params)
+        const requestOption = await this.requestOption()
+        const response = await fetch(url, requestOption)
+        const obj = await response.json()
+        return obj.files[0]
+    }
 
-	async list(path) {
-		if (gd.cache == undefined) {
-			gd.cache = {};
-		}
+    async list(path) {
+        if (gd.cache == undefined) {
+            gd.cache = {}
+        }
 
-		if (gd.cache[path]) {
-			return gd.cache[path];
-		}
+        if (gd.cache[path]) {
+            return gd.cache[path]
+        }
 
-		const id = await this.findPathId(path);
-		const obj = await this._ls(id);
-		if (obj.files && obj.files.length > 1000) {
-			gd.cache[path] = obj;
-		}
-		return obj;
-	}
+        const id = await this.findPathId(path)
+        const obj = await this._ls(id)
+        if (obj.files && obj.files.length > 1000) {
+            gd.cache[path] = obj
+        }
+        return obj
+    }
 
-	async _ls(parent) {
-		if (parent == undefined) {
-			return null;
-		}
-		const files = [];
-		let pageToken;
-		let obj;
-		const params = { 'includeItemsFromAllDrives': true, 'supportsAllDrives': true };
-		params.q = `'${parent}' in parents and trashed = false and mimeType != 'application/vnd.google-apps.shortcut'`;
-		params.orderBy = 'folder,name,modifiedTime desc';
-		params.fields = 'nextPageToken, files(id, name, mimeType, size , modifiedTime)';
-		params.pageSize = 1000;
+    async _ls(parent) {
+        if (parent == undefined) {
+            return null
+        }
+        const files = []
+        let pageToken
+        let obj
+        const params = {
+            includeItemsFromAllDrives: true,
+            supportsAllDrives: true,
+        }
+        params.q = `'${parent}' in parents and trashed = false and mimeType != 'application/vnd.google-apps.shortcut'`
+        params.orderBy = 'folder,name,modifiedTime desc'
+        params.fields =
+            'nextPageToken, files(id, name, mimeType, size , modifiedTime)'
+        params.pageSize = 1000
 
-		do {
-			if (pageToken) {
-				params.pageToken = pageToken;
-			}
-			let url = 'https://www.googleapis.com/drive/v3/files';
-			url += '?' + this.enQuery(params);
-			const requestOption = await this.requestOption();
-			const response = await fetch(url, requestOption);
-			obj = await response.json();
-			files.push(...obj.files);
-			pageToken = obj.nextPageToken;
-		}
-		while (pageToken);
+        do {
+            if (pageToken) {
+                params.pageToken = pageToken
+            }
+            let url = 'https://www.googleapis.com/drive/v3/files'
+            url += '?' + this.enQuery(params)
+            const requestOption = await this.requestOption()
+            const response = await fetch(url, requestOption)
+            obj = await response.json()
+            files.push(...obj.files)
+            pageToken = obj.nextPageToken
+        } while (pageToken)
 
-		obj.files = files;
-		return obj;
-	}
+        obj.files = files
+        return obj
+    }
 
-	async findPathId(path) {
-		let c_path = '/';
-		let c_id = this.paths[c_path];
+    async findPathId(path) {
+        let c_path = '/'
+        let c_id = this.paths[c_path]
 
-		const arr = path.trim('/').split('/');
-		for (const name of arr) {
-			c_path += name + '/';
-			if (typeof this.paths[c_path] == 'undefined') {
-				const id = await this._findDirId(c_id, name);
-				this.paths[c_path] = id;
-			}
+        const arr = path.trim('/').split('/')
+        for (const name of arr) {
+            c_path += name + '/'
+            if (typeof this.paths[c_path] == 'undefined') {
+                const id = await this._findDirId(c_id, name)
+                this.paths[c_path] = id
+            }
 
-			c_id = this.paths[c_path];
-			if (c_id == undefined || c_id == null) {
-				break;
-			}
-		}
-		return this.paths[path];
-	}
+            c_id = this.paths[c_path]
+            if (c_id == undefined || c_id == null) {
+                break
+            }
+        }
+        return this.paths[path]
+    }
 
-	async _findDirId(parent, name) {
-		name = decodeURIComponent(name).replace(/\'/g, '\\\'');
+    async _findDirId(parent, name) {
+        name = decodeURIComponent(name).replace(/\'/g, "\\'")
 
-		if (parent == undefined) {
-			return null;
-		}
+        if (parent == undefined) {
+            return null
+        }
 
-		let url = 'https://www.googleapis.com/drive/v3/files';
-		const params = { 'includeItemsFromAllDrives': true, 'supportsAllDrives': true };
-		params.q = `'${parent}' in parents and mimeType = 'application/vnd.google-apps.folder' and name = '${name}' and trashed = false and mimeType != 'application/vnd.google-apps.shortcut'`;
-		params.fields = 'nextPageToken, files(id, name, mimeType)';
-		url += '?' + this.enQuery(params);
-		const requestOption = await this.requestOption();
-		const response = await fetch(url, requestOption);
-		const obj = await response.json();
-		if (obj.files[0] == undefined) {
-			return null;
-		}
-		return obj.files[0].id;
-	}
+        let url = 'https://www.googleapis.com/drive/v3/files'
+        const params = {
+            includeItemsFromAllDrives: true,
+            supportsAllDrives: true,
+        }
+        params.q = `'${parent}' in parents and mimeType = 'application/vnd.google-apps.folder' and name = '${name}' and trashed = false and mimeType != 'application/vnd.google-apps.shortcut'`
+        params.fields = 'nextPageToken, files(id, name, mimeType)'
+        url += '?' + this.enQuery(params)
+        const requestOption = await this.requestOption()
+        const response = await fetch(url, requestOption)
+        const obj = await response.json()
+        if (obj.files[0] == undefined) {
+            return null
+        }
+        return obj.files[0].id
+    }
 
-	async accessToken() {
-		if (this.authConfig.expires == undefined || this.authConfig.expires < Date.now()) {
-			const obj = await this.fetchAccessToken();
-			if (obj.access_token != undefined) {
-				this.authConfig.accessToken = obj.access_token;
-				this.authConfig.expires = Date.now() + 3500 * 1000;
-			}
-		}
-		return this.authConfig.accessToken;
-	}
+    async accessToken() {
+        if (
+            this.authConfig.expires == undefined ||
+            this.authConfig.expires < Date.now()
+        ) {
+            const obj = await this.fetchAccessToken()
+            if (obj.access_token != undefined) {
+                this.authConfig.accessToken = obj.access_token
+                this.authConfig.expires = Date.now() + 3500 * 1000
+            }
+        }
+        return this.authConfig.accessToken
+    }
 
-	async fetchAccessToken() {
-		const url = 'https://www.googleapis.com/oauth2/v4/token';
-		const headers = {
-			'Content-Type': 'application/x-www-form-urlencoded',
-		};
-		const post_data = {
-			'client_id': this.authConfig.client_id,
-			'client_secret': this.authConfig.client_secret,
-			'refresh_token': this.authConfig.refresh_token,
-			'grant_type': 'refresh_token',
-		};
-		const requestOption = {
-			'method': 'POST',
-			'headers': headers,
-			'body': this.enQuery(post_data),
-		};
-		const response = await fetch(url, requestOption);
-		return await response.json();
-	}
+    async fetchAccessToken() {
+        const url = 'https://www.googleapis.com/oauth2/v4/token'
+        const headers = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        }
+        const post_data = {
+            client_id: this.authConfig.client_id,
+            client_secret: this.authConfig.client_secret,
+            refresh_token: this.authConfig.refresh_token,
+            grant_type: 'refresh_token',
+        }
+        const requestOption = {
+            method: 'POST',
+            headers: headers,
+            body: this.enQuery(post_data),
+        }
+        const response = await fetch(url, requestOption)
+        return await response.json()
+    }
 
-	async requestOption(headers = {}, method = 'GET') {
-		const accessToken = await this.accessToken();
-		headers['authorization'] = 'Bearer ' + accessToken;
-		return { 'method': method, 'headers': headers };
-	}
+    async requestOption(headers = {}, method = 'GET') {
+        const accessToken = await this.accessToken()
+        headers['authorization'] = 'Bearer ' + accessToken
+        return { method: method, headers: headers }
+    }
 
-	enQuery(data) {
-		const ret = [];
-		for (const d in data) {
-			ret.push(encodeURIComponent(d) + '=' + encodeURIComponent(data[d]));
-		}
-		return ret.join('&');
-	}
+    enQuery(data) {
+        const ret = []
+        for (const d in data) {
+            ret.push(encodeURIComponent(d) + '=' + encodeURIComponent(data[d]))
+        }
+        return ret.join('&')
+    }
 
-	sleep(ms) {
-		return new Promise(function (resolve, reject) {
-			let i = 0;
-			setTimeout(function () {
-				i++;
-				if (i >= 2) reject(new Error('i>=2'));
-				else resolve(i);
-			}, ms);
-		});
-	}
+    sleep(ms) {
+        return new Promise(function (resolve, reject) {
+            let i = 0
+            setTimeout(function () {
+                i++
+                if (i >= 2) reject(new Error('i>=2'))
+                else resolve(i)
+            }, ms)
+        })
+    }
 }
 
 String.prototype.trim = function (char) {
-	if (char) {
-		return this.replace(new RegExp('^\\' + char + '+|\\' + char + '+$', 'g'), '');
-	}
-	return this.replace(/^\s+|\s+$/g, '');
-};
+    if (char) {
+        return this.replace(
+            new RegExp('^\\' + char + '+|\\' + char + '+$', 'g'),
+            ''
+        )
+    }
+    return this.replace(/^\s+|\s+$/g, '')
+}
--- themes/classic/app.js
+++ themes/classic/app.js
@@ -12,141 +12,149 @@ td.file-name:hover{text-decoration:underline}
 th.file-size:hover{text-decoration:underline}
 td.file-size:hover{text-decoration:underline}
 th, td {padding-right:10px}
-</style>`);
-
+</style>`)
 
 function init() {
-	document.siteName = $('title').html();
-	var html = `
+    document.siteName = $('title').html()
+    var html = `
 	<h1 id="heading">Index of <?php echo urldecode($path);?></h1>
 	<table id="table">
 	</table>
 	<footer>&copy; 2023 <i id="host"></i>.</footer>
-	`;
-	$('body').html(html);
-	document.getElementById("host").innerHTML = window.location.hostname;
+	`
+    $('body').html(html)
+    document.getElementById('host').innerHTML = window.location.hostname
 }
 
 function render(path) {
-	if (path.indexOf("?") > 0) {
-		path = path.substr(0,path.indexOf("?"));
-	}
-	title(path);
-	nav(path);
-	if (path.substr(-1) == '/') {
-		list(path);
-	} else {
-		file(path);
-	}
+    if (path.indexOf('?') > 0) {
+        path = path.substr(0, path.indexOf('?'))
+    }
+    title(path)
+    nav(path)
+    if (path.substr(-1) == '/') {
+        list(path)
+    } else {
+        file(path)
+    }
 }
 
 function title(path) {
-	path = decodeURI(path);
-	var segment = "";
-	var lastSegment = "";
+    path = decodeURI(path)
+    var segment = ''
+    var lastSegment = ''
 
-	var segment = path.split('/');
-	var lastSegment = segment.pop() || segment.pop();
-	$('title').html(document.siteName + ' - ' + lastSegment);
+    var segment = path.split('/')
+    var lastSegment = segment.pop() || segment.pop()
+    $('title').html(document.siteName + ' - ' + lastSegment)
 }
 
 function nav(path) {
-	path = decodeURI(path);
-	var segment = "";
-	var sep = "";
-	var lastSegment = "";
+    path = decodeURI(path)
+    var segment = ''
+    var sep = ''
+    var lastSegment = ''
 
-	var segment = path.split('/');
-	var lastSegment = segment.pop() || segment.pop();
-	segment.forEach((segment) => sep += "../");
-	$('#heading').html('Index of' + ' ' + sep + formatName(lastSegment));
+    var segment = path.split('/')
+    var lastSegment = segment.pop() || segment.pop()
+    segment.forEach((segment) => (sep += '../'))
+    $('#heading').html('Index of' + ' ' + sep + formatName(lastSegment))
 }
 
 function list(path) {
-	var content = `<tr><th class="file-name">Name</th><th class="file-size">Size</th></tr>`;
+    var content = `<tr><th class="file-name">Name</th><th class="file-size">Size</th></tr>`
 
-	if (path != '/') {
-		var up = path.split('/');
-		up.pop();up.pop();
-		up = up.join('/')+'/';
-		content += `
+    if (path != '/') {
+        var up = path.split('/')
+        up.pop()
+        up.pop()
+        up = up.join('/') + '/'
+        content += `
 		<tr>
 		<td class="file-name">
 		<a href="${up}">..</a>
 		</td>
 		<td class="file-size"></td>
 		</tr>
-		`;	
-	}
-	$('#table').html(content);
-	
-	$.post(path, function (data) {
-		var obj = jQuery.parseJSON(data);
-		if (typeof obj != 'null') {
-			list_files(path, obj.files);
-		} else {
-			list(path);
-		}
-	});
+		`
+    }
+    $('#table').html(content)
+
+    $.post(path, function (data) {
+        var obj = jQuery.parseJSON(data)
+        if (typeof obj != 'null') {
+            list_files(path, obj.files)
+        } else {
+            list(path)
+        }
+    })
 }
 
 function list_files(path, files) {
-	html = "";
-	for (i in files) {
-		var item = files[i];
-		item['name'] = item['name'];
-		item['mimeType'] = item['mimeType'];
-		/** Handle directory size **/
-		if (item['size'] == undefined) {
-			item['size'] = null;
-		}
-		item['size'] = formatFileSize(item['size']);
-		if (item['mimeType'] == 'application/vnd.google-apps.folder') {
-			var p = path+item.name+'/';
-			html +=`
+    html = ''
+    for (i in files) {
+        var item = files[i]
+        item['name'] = item['name']
+        item['mimeType'] = item['mimeType']
+        /** Handle directory size **/
+        if (item['size'] == undefined) {
+            item['size'] = null
+        }
+        item['size'] = formatFileSize(item['size'])
+        if (item['mimeType'] == 'application/vnd.google-apps.folder') {
+            var p = path + item.name + '/'
+            html += `
 				<tr>
 					<td class="file-name"><a href="${p}">${item['name']}/</a></td>
 					<td class="file-size">${item['size']}</td>
 				</tr>
-			`;
-		} else {
-			var p = path+item.name;
-			html += `
+			`
+        } else {
+            var p = path + item.name
+            html += `
 				<tr>
 					<td class="file-name"><a href="${p}">${item['name']}</a></td>
 					<td class="file-size">${item['size']}</td>
 				</tr>
-			`;
-		}
-	}
-	$('#table').append(html);
+			`
+        }
+    }
+    $('#table').append(html)
 }
 
 function localtime(utc_datetime) {
-	var T_pos = utc_datetime.indexOf('T');
-	var Z_pos = utc_datetime.indexOf('Z');
-	var year_month_day = utc_datetime.substr(0,T_pos);
-	var hour_minute_second = utc_datetime.substr(T_pos+1,Z_pos-T_pos-1);
-	var new_datetime = year_month_day + " " + hour_minute_second;
+    var T_pos = utc_datetime.indexOf('T')
+    var Z_pos = utc_datetime.indexOf('Z')
+    var year_month_day = utc_datetime.substr(0, T_pos)
+    var hour_minute_second = utc_datetime.substr(T_pos + 1, Z_pos - T_pos - 1)
+    var new_datetime = year_month_day + ' ' + hour_minute_second
 
-	timestamp = new Date(Date.parse(new_datetime));
-	timestamp = timestamp.getTime();
-	timestamp = timestamp/1000;
+    timestamp = new Date(Date.parse(new_datetime))
+    timestamp = timestamp.getTime()
+    timestamp = timestamp / 1000
 
-	var unixtimestamp = timestamp + 7 * 60 * 60;
+    var unixtimestamp = timestamp + 7 * 60 * 60
 
-	var unixtimestamp = new Date(unixtimestamp*1000);
-	var year = 1900 + unixtimestamp.getYear();
-	var month = "0" + (unixtimestamp.getMonth() + 1);
-	var date = "0" + unixtimestamp.getDate();
-	var hour = "0" + unixtimestamp.getHours();
-	var minute = "0" + unixtimestamp.getMinutes();
-	var second = "0" + unixtimestamp.getSeconds();
-	return year + "-" + month.substring(month.length-2, month.length)
-	+ "-" + date.substring(date.length-2, date.length)
-	+ " " + hour.substring(hour.length-2, hour.length)
-	+ ":" + minute.substring(minute.length-2, minute.length)
-	+ ":" + second.substring(second.length-2, second.length);
+    var unixtimestamp = new Date(unixtimestamp * 1000)
+    var year = 1900 + unixtimestamp.getYear()
+    var month = '0' + (unixtimestamp.getMonth() + 1)
+    var date = '0' + unixtimestamp.getDate()
+    var hour = '0' + unixtimestamp.getHours()
+    var minute = '0' + unixtimestamp.getMinutes()
+    var second = '0' + unixtimestamp.getSeconds()
+    return (
+        year +
+        '-' +
+        month.substring(month.length - 2, month.length) +
+        '-' +
+        date.substring(date.length - 2, date.length) +
+        ' ' +
+        hour.substring(hour.length - 2, hour.length) +
+        ':' +
+        minute.substring(minute.length - 2, minute.length) +
+        ':' +
+        second.substring(second.length - 2, second.length)
+    )
 }
 
 function formatFileSize(bytes, decimals = 0) {
@@ -162,28 +170,28 @@ function formatFileSize(bytes, decimals = 0) {
 }
 
 function formatName(f) {
-	var length = 20;
-	var f = f.length > length ?
-		f.substring(0, length - 3) + "..." + f.substring(f.length - 5):
-		f;
-	return f;
+    var length = 20
+    var f =
+        f.length > length
+            ? f.substring(0, length - 3) + '...' + f.substring(f.length - 5)
+            : f
+    return f
 }
 
-window.onpopstate = function(){
-	var path = window.location.pathname;
-	render(path);
+window.onpopstate = function () {
+    var path = window.location.pathname
+    render(path)
 }
 
+$(function () {
+    init()
+    var path = window.location.pathname
+    $('body').on('click', '.folder', function () {
+        var url = $(this).attr('href')
+        history.pushState(null, null, url)
+        render(url)
+        return false
+    })
 
-$(function() {
-	init();
-	var path = window.location.pathname;
-	$("body").on("click",'.folder',function() {
-		var url = $(this).attr('href');
-		history.pushState(null, null, url);
-		render(url);
-		return false;
-	});
-
-	render(path);
-});
+    render(path)
+})
